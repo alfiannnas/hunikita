f33d2cbe5730ce8c362fa2031006c9ae17c50771
Alfian Kafilah Baits <alfiank@stellargeloratech.com>
2025-03-27 20:22:59 +0700

    fix

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -102,7 +102,6 @@ const AdminLogin = () => {
                   onClick={() => setShowPassword(!showPassword)}
                   className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
                 >
-                  👁
                 </button>
               </div>
             </div>
