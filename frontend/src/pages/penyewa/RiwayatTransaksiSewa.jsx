24f4e630d948f6b1944ca547b927bf235bd3a88e
Alfian Kafilah Ba'its <alfian.baits@gmail.com>
2025-06-15 14:38:10 +0700

    remove

--- frontend/src/pages/penyewa/RiwayatTransaksiSewa.jsx
+++ frontend/src/pages/penyewa/RiwayatTransaksiSewa.jsx
@@ -162,20 +162,11 @@ const RiwayatTransaksiSewa = () => {
                                         </td>
                                         <td className="py-3 flex space-x-2">
                                             <Link
-                                                to={`/properties/edit/${item.id}`}
+                                                to={`/detail-sewa/${item.uuid}`}
                                                 className="p-2 text-blue-600 border border-blue-600 rounded-md hover:bg-blue-600 hover:text-white transition"
                                             >
                                                 <Edit className="w-5 h-5" />
                                             </Link>
-                                            <button
-                                                onClick={() => {
-                                                    setIdProperty(item.id);
-                                                    setIsAlertOpen(true);
-                                                }}
-                                                className="p-2 text-red-600 border border-red-600 rounded-md hover:bg-red-600 hover:text-white transition"
-                                            >
-                                                <Trash2 className="w-5 h-5" />
-                                            </button>
                                         </td>
                                     </tr>
                                 ))
